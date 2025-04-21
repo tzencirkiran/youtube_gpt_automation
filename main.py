e9e24cf623ae48481aa9b2f8e83ead12deaac324
tzencirkiran <talhazencirkiran@marun.edu.tr>
2025-04-21 16:41:29 +0300

    Mini tests for new class structure done, np it seems. But ytTranscriptApi requires much better exception handling

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from ytdataApi import search, pull_comments, fetch_video_titles, fetch_video_ids_pl
-from ytTranscriptApi import fetch_transcript
-from fileHandler import merge_transcript_comments, save_transcript_comments, \
-    read_transcript_comments, VideoIdHandler
-import pandas as pd
+from ytdataApi import YoutubeDataAPI
+from fileHandler import FileHandler, VideoIdHandler
+from ytTranscriptApi import fetch_transcript_comments
 import json
 
-# For Youtube Data API, I need a method to gather video ids from url links
 
 
 # I need to merge some methods for less complexity
+print("1")
+video_ids = YoutubeDataAPI.fetch_video_ids_pl("PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU")
 
+print("2")
+VideoIdHandler.save_video_ids(video_ids=video_ids)
+
+print("3")
+tc_comment_dict = fetch_transcript_comments(VideoIdHandler.read_video_ids())
+
+print("4")
+
+print("5")
+FileHandler.save_transcript_comments(tc_comment_dict)
 
 # I need to put methods of different files to classes for increased modularity
 
--- ytTranscriptApi.py
+++ ytTranscriptApi.py
@@ -1,4 +1,5 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from ytdataApi import YoutubeDataAPI
 
 # Create an object for YouTubeTranscriptApi
 ytt_api = YouTubeTranscriptApi()
@@ -16,7 +17,11 @@ def fetch_transcript(video_ids):
         
     id_transcript_dict = {}
     for video_id in video_ids:
-        fetched_transcript = ytt_api.fetch(video_id, languages=["en", "tr"])
+        try:    
+            fetched_transcript = ytt_api.fetch(video_id, languages=["en", "tr"])
+        except:
+            print(f"Something went wrong when the program was fetching \
+                  {YoutubeDataAPI.fetch_video_titles(video_id)}")
 
         transcript_text_list = []
         for snippet in fetched_transcript:
@@ -28,4 +33,28 @@ def fetch_transcript(video_ids):
     return id_transcript_dict
     
     
-
+# Fetches and merges trancripts and comments into a dictionary, video_id being 
+# keys. It calls both fetch_transcript() and pull_comments() without 
+# seperate calls for each
+def fetch_transcript_comments(video_ids):
+    if isinstance(video_ids, list):
+        pass
+    elif isinstance(video_ids, str):
+        video_ids = [video_ids]
+    elif not isinstance(video_ids, (list, str)):
+        raise TypeError("video_ids should be list or string")
+    
+    
+    merged_dict = {}
+    for video_id in video_ids:
+        merged_dict[video_id] = {
+            # fetching videoTitle is insufficient, 
+            # bc the method can gather multiple ids at a time
+            "videoTitle": YoutubeDataAPI.fetch_video_titles(video_id)[0],  # list of one title
+                "data": {
+                    "transcript": fetch_transcript(video_id)[video_id],
+                    "comments": YoutubeDataAPI.pull_comments(video_id)[video_id]
+                }
+        }
+  
+    return merged_dict

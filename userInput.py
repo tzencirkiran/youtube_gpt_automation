d1827d5e9873c8643b8f6d33439ea33cdf57e756
tzencirkiran <talhazencirkiran@marun.edu.tr>
2025-04-21 19:03:41 +0300

    Created a new file for UserInput class

---
+++ userInput.py
@@ -0,0 +1,29 @@
+from ytdataApi import YoutubeDataAPI
+from ytTranscriptApi import fetch_transcript_comments
+from fileHandler import FileHandler
+
+class UserInput:
+    # Fetches and saves transcript and comments to trancript_comments.json
+    def trancript_comments_to_json(vid_pl_id):
+        transcript_comments_dict = {}
+        # Decide whether input is playlist or not, then get their ids
+        if vid_pl_id.str.startswith("PL"):
+            # Storing ids in video_ids, store trancript_comments in 
+            # transcript_comments_dict and save it to json file
+            video_ids = YoutubeDataAPI.fetch_video_ids_pl(vid_pl_id)
+            transcript_comments_dict = fetch_transcript_comments(video_ids)
+            FileHandler.save_transcript_comments(transcript_comments_dict)
+        else:
+            # Fetch video's trancript and comments to to dictionary
+            transcript_comments_dict = fetch_transcript_comments(vid_pl_id)
+            # Save dictionary to json file
+            FileHandler.save_transcript_comments(transcript_comments_dict)
+
+    # Reads and returns transcript_comments.json
+    def read_transcript_comments():
+        transcript_comments = FileHandler.read_transcript_comments()
+        return transcript_comments
+    
+    # Takes user input of api_key and writes it to .env (YT_API_KEY)
+    def set_youtube_api_key(user_api_key):
+        FileHandler.setup_env(user_api_key)
